d2967b8bcd20908fda5a420b6fa0a56fade29fcd
Jashon Osala <jashonosala@gmail.com>
2022-07-02 14:17:04 +0000

    Workspace transfer

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ fi
 BETTY_STYLE="betty-style"
 BETTY_DOC="betty-doc"
 BETTY_WRAPPER="betty"
+MAIN_TEMPLATE="main"
+BETTY_TOOL="createbetty"
 
 APP_PATH="/opt/betty"
 BIN_PATH="/usr/local/bin"
@@ -26,6 +28,18 @@ chmod +x "${APP_PATH}/${BETTY_STYLE}"
 chmod +x "${APP_PATH}/${BETTY_DOC}"
 chmod +x "${APP_PATH}/${BETTY_WRAPPER}"
 
+echo -e "Adding betty to bin.."
+
+cp "${BETTY_WRAPPER}.sh" "${BIN_PATH}/${BETTY_WRAPPER}"
+
+echo -e "Adding c template to app path.."
+
+cp "${MAIN_TEMPLATE}.c" "${APP_PATH}/${MAIN_TEMPLATE}.c"
+
+echo -e "Installing createbetty tool.."
+
+cp "${BETTY_TOOL}" "${BIN_PATH}/${BETTY_TOOL}"
+
 ln -s "${APP_PATH}/${BETTY_STYLE}" "${BIN_PATH}/${BETTY_STYLE}"
 ln -s "${APP_PATH}/${BETTY_DOC}" "${BIN_PATH}/${BETTY_DOC}"
 ln -s "${APP_PATH}/${BETTY_WRAPPER}" "${BIN_PATH}/${BETTY_WRAPPER}"
@@ -37,6 +51,7 @@ mkdir -p "${MAN_PATH}"
 cp "man/betty.1" "${MAN_PATH}"
 cp "man/${BETTY_STYLE}.1" "${MAN_PATH}"
 cp "man/${BETTY_DOC}.1" "${MAN_PATH}"
+cp "man/${BETTY_TOOL}.1" "${MAN_PATH}"
 
 echo -e "Updating man database.."
 
